242576c6631938075efa2d8df7a41b4db71c2afe
Lizotte <e1639345@cmaisonneuve.qc.ca>
2019-02-21 10:47:30 -0500

    Mega mise à jour ajout mobile encore buggé

--- js/triMedias.js
+++ js/triMedias.js
@@ -1,12 +1,20 @@
- (function() {
-            for (let e of document.querySelectorAll("input[type='checkbox']")) {
-                e.addEventListener("change", function() {
+ (function () {
+     for (let e of document.querySelectorAll("input[type='checkbox']")) {
+         e.addEventListener("change", function () {
+             if (e.parentElement.classList.contains("boutonCocher")) {
+                 e.parentElement.classList.remove("boutonCocher")
+                 e.parentElement.classList.add("boutonDecocher")
 
-                    for (let i of document.querySelectorAll(`div[class=${e.value}]`)) {
-                        i.style.display = (e.checked) ? "flex" : "none";
-                    }
+             } else if (e.parentElement.classList.contains("boutonDecocher")) {
+                 e.parentElement.classList.add("boutonCocher")
+                 e.parentElement.classList.remove("boutonDecocher")
+
+             }
+             for (let i of document.querySelectorAll(`div[class=${e.value}]`)) {
+                 i.style.display = (e.checked) ? "flex" : "none";
+             }
 
 
-                });
-            }
-        })()
+         });
+     }
+ })()
